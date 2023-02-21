fb2b6d6fe86a113bdfa9237eea8f67e0b1abc876
down.jung <down.jung@sk.com>
2023-02-21 11:10:21 +0900

    Revert "ExceptionHandler Ex"
    This reverts commit ba006440109643dafa00e8a53c2e37c8a7fb370e.

--- src/main/kotlin/com/group/libraryapp/controller/user/UserController.kt
+++ src/main/kotlin/com/group/libraryapp/controller/user/UserController.kt
@@ -4,7 +4,6 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import com.group.libraryapp.dto.user.response.UserResponse
 import com.group.libraryapp.service.user.UserService
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -16,15 +15,6 @@ class UserController(
   fun saveUser(@RequestBody request: UserCreateRequest, @One one: Long) {
     println(one) // 1이었으면 좋겠다. ArgumentResolver
     userService.saveUser(request)
-  }
-
-  /**
-   * 단점 : controller마다 만들어야함
-   * @RestControllerAdvice 로 전체 가능 but, 필터는 타지 않음
-   */
-  @ExceptionHandler(IllegalArgumentException::class)
-  fun handleException() : ResponseEntity<String> {
-    return ResponseEntity.ok("sorry");
   }
 
   @GetMapping("/user")
